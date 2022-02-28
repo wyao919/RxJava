2f567125d798dc71c03961e14ec5ba416eef7a2f
willy <willyyao@outlook.com>
2022-02-28 14:13:38 -0800

    finished Defer

---
+++ FirstProject/src/main/java/ObservableRangeAndDefer.java
@@ -0,0 +1,24 @@
+import io.reactivex.Observable;
+
+public class ObservableRangeAndDefer {
+
+    private static int start = 5, count =2;
+
+    public static void main(String[] args) {
+//        Observable<Integer> observable = Observable.range(0,10);
+//        observable.subscribe(System.out::println);
+
+
+        Observable<Integer> observable = Observable.defer(() -> {
+
+            System.out.println("New observable is created with start = "+ start + " and count " + count);
+            return Observable.range(start, count);
+        });
+        observable.subscribe(integer -> System.out.println("Observer 1: " + integer));
+
+        count =3;
+
+        observable.subscribe(integer -> System.out.println("Observer 2: " + integer));
+
+    }
+}
